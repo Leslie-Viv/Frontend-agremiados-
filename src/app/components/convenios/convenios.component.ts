ca21d7091fbc9beec48cf8b7302c9caa020ce600
Leslie-Viv <84611239+Leslie-Viv@users.noreply.github.com>
2023-12-11 01:50:43 -0600

    botones

--- src/app/components/convenios/convenios.component.ts
+++ src/app/components/convenios/convenios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-convenios',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConveniosComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
-  ngOnInit() {}
+    ngOnInit(): void {
+    }
+
+    logout() {
+      this.router.navigate(['/login']);
+    }
 
 }
--- src/app/components/formatos/formatos.component.ts
+++ src/app/components/formatos/formatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formatos',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormatosComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit() {}
+
+  logout() {
+    this.router.navigate(['/login']);
+  }
 
 }
